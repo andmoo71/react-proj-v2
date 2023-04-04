68e36a3341761b650101d00f56af3424a86e457c
andmoo71 <andrewmooree@gmail.com>
2023-04-04 09:26:04 -0700

    ducks

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({"users": ["rafael"]})
+});
+
+app.listen(8000, () => {
+  console.log("listening on 5000");
+});
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Card = ({ name, email, id }) => {
     return (
-      <div className="tc bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5">
-        <img src={`https://picsum.photos/id/${id}/200/200`} alt="placeholder" />
+      <div className="tc bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5 dim">
+        <img
+          src={`https://random-d.uk/api/${id}.jpg`}
+          alt="placeholder"
+          width="300"
+          height="200"
+        />
         <div>
           <h2>{name}</h2>
           <p>{email}</p>
